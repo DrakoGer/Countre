95242956319bd9a9bc9a6b3deebeeb2bed68b750
Yura <gije.ger@gmail.com>
2024-10-15 20:04:32 +0400

    Исправленны рекомендации ревьюера

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,71 +9,71 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private var zero: Int = 0
-    private var historiText: String = "История изменений"
+    @IBOutlet private weak var historyLabel: UILabel!
+    @IBOutlet private weak var textView: UITextView!
+    @IBOutlet private weak var counterLabel: UILabel!
     
-    @IBOutlet weak var historyLabel: UILabel!
-    @IBOutlet weak var textView: UITextView!
-    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet private weak var plusButton: UIButton!
+    @IBOutlet private weak var minusButton: UIButton!
+    @IBOutlet private weak var refreshButton: UIButton!
     
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var minusButton: UIButton!
-    @IBOutlet weak var refreshButton: UIButton!
+    private var counterValue: Int = 0
+    private var historyText: String = "История изменений"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        scoreLabel.text = "Значение счетчика: \(zero)"
+        counterLabel.text = "Значение счетчика: \(counterValue)"
         textView.isEditable = false
-        historyLabel.text = historiText
+        historyLabel.text = historyText
     }
     
-    @IBAction func plusButtonTapped(_ sender: Any) {
-        zero += 1
+    @IBAction private func plusButtonTapped(_ sender: Any) {
+        counterValue += 1
         
-        addToHistory("Значение изменино на +1")
-        updateScore()
+        UpdateHistory("Значение изменено на +1")
+        updateCounterValue()
         
-        if zero > 0 {
+        if counterValue > 0 {
             minusButton.isEnabled = true
             refreshButton.isEnabled = true
         }
     }
     
-    @IBAction func minusButtonTapped(_ sender: Any) {
-        if zero > 0 {
-            zero -= 1
+    @IBAction private func minusButtonTapped(_ sender: Any) {
+        if counterValue > 0 {
+            counterValue -= 1
             
-            addToHistory("Значение изменино на -1")
-            updateScore()
+            UpdateHistory("Значение изменено на -1")
+            updateCounterValue()
         } else {
-            addToHistory("Значение счётчика ниже 0, нельзя уменьшить ещё")
+            UpdateHistory("Значение счётчика ниже 0, нельзя уменьшить ещё")
             minusButton.isEnabled = false
         }
     }
     
-    @IBAction func refreshBuutonTapped(_ sender: Any) {
-        if zero > 0 {
-            zero = 0
+    @IBAction private func refreshButtonTapped(_ sender: Any) {
+        if counterValue > 0 {
+            counterValue = 0
             
             minusButton.isEnabled = false
             refreshButton.isEnabled = false
-
-            addToHistory("Значение сброшено до 0")
-            updateScore()
+            
+            UpdateHistory("Значение сброшено до 0")
+            updateCounterValue()
         } else {
-            if zero >= 0 {
-                addToHistory("Значение 0, нельзя сбросить")
+            if counterValue >= 0 {
+                UpdateHistory("Значение 0, нельзя сбросить")
                 refreshButton.isEnabled = false
             }
         }
     }
     
-    private func updateScore() {
-        scoreLabel.text = "Значение счетчика: \(zero)"
-        }
+    private func updateCounterValue() {
+        counterLabel.text = "Значение счетчика: \(counterValue)"
+    }
     
-    private func addToHistory(_ message: String) {
+    private func UpdateHistory(_ message: String) {
         let timeMessage = getCurrentTimestamp()
         textView.text = (textView.text ?? "") + "\n[\(timeMessage)]: \(message)"
         
